7da11fe31e23ab5d696bca8d045e15624beda5ed
KravchenkoKir <nkravcenko2@edu.riga.lv>
2022-11-30 19:21:10 +0200

    Better Gridding & Cleaner code

--- GUI.py
+++ GUI.py
@@ -13,13 +13,13 @@ m.title('Store')
 
 Label1 = Label(m, text="Choose an object or make a new one to work with.")
 
-Lb1 = Listbox(m)
+Listbox = Listbox(m)
 
 #method for entering the names of parts into the listbox
 def namesofParts():
-    Lb1.delete(0, END)
+    Listbox.delete(0, END)
     for curPart in range(len(existingParts)):
-        Lb1.insert (curPart, existingParts[curPart].model)
+        Listbox.insert (curPart, existingParts[curPart].model)
 
 namesofParts()
 
@@ -35,7 +35,7 @@ modelEntry = Entry(m)
 priceEntry = Entry(m)
 
 def DataExtraction():
-    dataIndex = Lb1.curselection()[0]
+    dataIndex = Listbox.curselection()[0]
     curPart = existingParts[dataIndex]
     typeEntry.delete(first=0, last=len(typeEntry.get()))
     modelEntry.delete(first=0, last=len(modelEntry.get()))
@@ -50,7 +50,7 @@ addItem = Checkbutton(m, text="Add as new part", variable = yes_no)
 #Commands for buttons responsible for either updating or adding new items
 def DataUpdate():
     if yes_no.get() == 0 :
-        dataIndex = Lb1.curselection()[0]
+        dataIndex = Listbox.curselection()[0]
         existingParts[dataIndex].type = typeEntry.get()
         existingParts[dataIndex].model = modelEntry.get()
         existingParts[dataIndex].price = priceEntry.get()
@@ -64,7 +64,7 @@ def DataUpdate():
 
 #Method for exporting
 def CreateCheque():
-    a.Export(existingParts[Lb1.curselection()[0]])
+    a.Export(existingParts[Listbox.curselection()[0]])
 
 showButton = Button(m, text = "Show", command=DataExtraction)
 updateButton = Button(m, text="Update", command=DataUpdate)
@@ -73,21 +73,22 @@ exportButton = Button(m,text="Export", command=CreateCheque)
 
 #Gridding
 Label1.grid(row=0, column=0)
-Lb1.grid(row = 1, column = 0, ipadx = 50)
+Listbox.grid(row = 1, column = 0, ipadx = 30)
 
 typeLabel.grid(row=2, column=0)
-typeEntry.grid(row=2, column=1, ipadx=50)
-modelLabel.grid(row=3, column=0)
+typeEntry.grid(row=3, column=0, ipadx=50)
 
-priceLabel.grid(row=4, column=0)
-modelEntry.grid(row=3, column=1, ipadx=50)
-priceEntry.grid(row=4, column=1, ipadx=50)
+modelLabel.grid(row=4, column=0)
+modelEntry.grid(row=5, column=0, ipadx=50)
 
-addItem.grid(row=0, column=1)
+priceLabel.grid(row=6, column=0)
+priceEntry.grid(row=7, column=0, ipadx=50)
 
-showButton.grid(row=0, column=2)
-updateButton.grid(row=0, column=3)
-exportButton.grid(row=1, column=2)
+addItem.grid(row=8, column=0)
+
+showButton.grid(row=9, column=0, ipadx=20)
+updateButton.grid(row=10, column=0, ipadx=15)
+exportButton.grid(row=11, column=0, ipadx=17)
 
 
 m.mainloop()
